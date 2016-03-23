27450db37de8d26814d3891aaad8be80966c60c5
Nicksax12 <nicksax12@hotmail.com>
2016-03-23 08:55:24 -0700

    added crossOrigin proxy

--- script.js
+++ script.js
@@ -2,21 +2,31 @@
 var movieTrailers =[];
 var movies = [];
 
+
+/**************************************************************************************************************
+						Get movie trailers from traileraddict(movie titles sourced in movielist.js)
+**************************************************************************************************************/
 // $.getJSON('http://api.traileraddict.com/?film=curious-case-benjamin-button&count=3' + '&callback=?', function(data){
 // 	alert(data.contents);
 // });
 
-// $.ajax({
-// 	method: 'GET',
-// 	dataType: 'xml',
-// 	url: 'http://api.traileraddict.com/?film=curious-case-benjamin-button&count=3',
-// 	success: function(data){
-// 		movieTrailers.push(data);  
-// 		console.log(data)
-// 	}
-// });
-// console.log(movieTrailers);
+$.ajax({
+	method: 'GET',
+	dataType: 'xml',
+	url: 'http://crossorigin.me/http://api.traileraddict.com/?film=curious-case-benjamin-button&count=3',
+	success: function(data){
+		movieTrailers.push(xmlToJson(data));  
+		console.log(data)
+	}
+});
+console.log(movieTrailers);
 
+
+
+/**************************************************************************************************************
+						Get movie info from omdb(movie titles sourced in movielist.js)
+**************************************************************************************************************/
+var itemsProcessed = 0;
 
 movieList.forEach(function(string){
 	var title = string.split(" ").join("+");
@@ -27,22 +37,27 @@ movieList.forEach(function(string){
 			if (data.Response == "True"){
 				movies.push(data);
 			}
+			itemsProcessed++;
+			if(itemsProcessed === movieList.length) {
+				placePosters();
+			}
 			// postPoster();
 		},
 		error: function(error){
 			console.log(error);
 		}
 	});
-})
+});
 
-window.setTimeout(function(){
+// Appends movie posters to page
+var placePosters = function(){
 	var posterRotation = 0;
 	for(var i = 0; i <= 8; i++){
-		var posterURL = movies[i].Poster;
-		$('a-scene').append("<a-curvedimage crossOrigin='Anonymous' src=" + posterURL + " radius='5' theta-length='40' height='6' rotation='0 " + posterRotation + " 0'</a-curvedimage>");
+		var posterURL = 'http://crossorigin.me/' + movies[i].Poster;
+		$('a-scene').append("<a-curvedimage src=" + posterURL + " radius='5' theta-length='40' height='6' rotation='0 " + posterRotation + " 0'</a-curvedimage>");
 		posterRotation += 40;
 	}
-}, 8000);
+};
 // 
 
 
@@ -68,3 +83,42 @@ window.setTimeout(function(){
 // $('#drama').click(function(){
 // 	filterByGenre('Drama');
 // });
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+	
+	// Create the return object
+	var obj = {};
+
+	if (xml.nodeType == 1) { // element
+		// do attributes
+		if (xml.attributes.length > 0) {
+		obj["@attributes"] = {};
+			for (var j = 0; j < xml.attributes.length; j++) {
+				var attribute = xml.attributes.item(j);
+				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+			}
+		}
+	} else if (xml.nodeType == 3) { // text
+		obj = xml.nodeValue;
+	}
+
+	// do children
+	if (xml.hasChildNodes()) {
+		for(var i = 0; i < xml.childNodes.length; i++) {
+			var item = xml.childNodes.item(i);
+			var nodeName = item.nodeName;
+			if (typeof(obj[nodeName]) == "undefined") {
+				obj[nodeName] = xmlToJson(item);
+			} else {
+				if (typeof(obj[nodeName].push) == "undefined") {
+					var old = obj[nodeName];
+					obj[nodeName] = [];
+					obj[nodeName].push(old);
+				}
+				obj[nodeName].push(xmlToJson(item));
+			}
+		}
+	}
+	return obj;
+};
